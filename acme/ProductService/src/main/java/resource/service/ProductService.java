bcf20b50eb5127455331ba9e85cccc223205bebc
Luís Neves <luismanuelneves5@gmail.com>
2023-04-03 23:36:52 +0100

    Migrate Product to Micro Service

---
+++ acme/ProductService/src/main/java/resource/service/ProductService.java
@@ -0,0 +1,26 @@
+package resource.service;
+
+import resource.dto.ProductDTO;
+import resource.dto.ProductDetailDTO;
+import resource.model.Product;
+
+import java.util.Optional;
+
+public interface ProductService {
+
+    Optional<ProductDTO> findBySku(final String sku);
+
+    Optional<Product> getProductBySku(final String sku );
+
+    Iterable<ProductDTO> findByDesignation(final String designation);
+
+    Iterable<ProductDTO> getCatalog();
+
+    ProductDetailDTO getDetails(final String sku);
+
+    ProductDTO create(final Product manager);
+
+    ProductDTO updateBySku(final String sku, final Product product);
+
+    void deleteBySku(final String sku);
+}
