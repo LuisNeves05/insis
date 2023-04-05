1d85dd8fd8bbebe72fdd929fc7d9a762a7ff341f
Luís Neves <luismanuelneves5@gmail.com>
2023-04-05 01:41:14 +0100

    Resolve Database Bugs | File system bug still exists
    Co-Authored-By: Jorge Pessoa <45247213+jorgepessoa6@users.noreply.github.com>

--- acme/ProductService/src/main/java/resource/property/FileStorageProperties.java
+++ acme/ProductService/src/main/java/resource/property/FileStorageProperties.java
@@ -1,9 +1,10 @@
 package resource.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "file")
-@Component
 public class FileStorageProperties {
 
     private String uploadDir;
--- acme/ProductService/src/main/java/resource/service/FileStorageService.java
+++ acme/ProductService/src/main/java/resource/service/FileStorageService.java
@@ -1,6 +1,7 @@
 package resource.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
